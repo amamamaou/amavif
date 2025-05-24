3f7bc0f233a8586b7b81b781b357e7255d5444c4
幸乃ととめ <41187086+amamamaou@users.noreply.github.com>
2025-05-24 15:18:49 +0900

    Improved processing

--- src/libs/convert.ts
+++ src/libs/convert.ts
@@ -1,41 +1,14 @@
-import { invoke } from '@tauri-apps/api/core'
+import { convertFileSrc, invoke } from '@tauri-apps/api/core'
 import { sleep } from '@/libs/utility'
 import useImageStore from '@/store/image'
 
+/** 変換データ */
 type ConvertedData = [string, string][]
-
-/** 変換後のデータ整理 */
-async function setConverted(
-  data: ConvertedData,
-  removeItem: boolean = false,
-): Promise<void> {
-  const image = useImageStore()
-
-  for (const [uuid, path] of data) {
-    const orig = image.standby.get(uuid)
-    if (!orig) continue
-
-    const dirPath = orig.directory ? orig.directory + '/' : ''
-    const afterSize = await invoke<number>('get_file_size', { path })
-
-    image.complete.set(uuid, {
-      path,
-      fileName: `${dirPath}${orig.baseName}.${image.format}`,
-      baseName: orig.baseName,
-      mimeType: `image/${image.format}`,
-      size: {
-        before: orig.size.before,
-        after: afterSize,
-      },
-    })
-
-    if (removeItem) image.standby.delete(uuid)
-  }
-}
 
 /** 画像を変換する */
 export default async function convertImage(): Promise<boolean> {
   const image = useImageStore()
+  let result = true
 
   image.isProcessing = true
 
@@ -47,41 +20,50 @@ export default async function convertImage(): Promise<boolean> {
     })
 
   // 変換開始
-  const result = await invoke<ConvertedData>('convert_images', {
+  const converted = await invoke<ConvertedData>('convert_images', {
     fileData,
     format: image.format,
     quality: image.quality,
     output: image.output,
   }).catch((error) => {
+    result = false
+
     // eslint-disable-next-line no-console
     if (error instanceof Error) console.error(error.message)
-    return null
-  })
 
-  // 変換中にエラーが出た場合
-  if (result === null || result.length < fileData.length) {
-    const existFiles = await invoke<ConvertedData>('check_existing_files', {
+    // 変換成功したものだけ取得する
+    return invoke<ConvertedData>('check_existing_files', {
       fileData,
       output: image.output,
     })
+  })
 
-    await setConverted(existFiles, true)
-    await sleep(400)
+  // データ整理
+  for (const [uuid, path] of converted) {
+    const orig = image.standby.get(uuid)
+    if (!orig) continue
 
-    image.isProcessing = false
+    const dirPath = orig.directory ? orig.directory + '/' : ''
+    const afterSize = await invoke<number>('get_file_size', { path })
 
-    return false
+    image.complete.set(uuid, {
+      path,
+      fileName: `${dirPath}${orig.baseName}.${image.format}`,
+      baseName: orig.baseName,
+      mimeType: `image/${image.format}`,
+      fileSrc: convertFileSrc(path),
+      size: {
+        before: orig.size.before,
+        after: afterSize,
+      },
+    })
+
+    image.standby.delete(uuid)
   }
-
-  // 変換後のデータ整理
-  await setConverted(result)
-
-  // 変換対象リストを空にする
-  image.standby.clear()
 
   await sleep(400)
 
   image.isProcessing = false
 
-  return true
+  return result
 }
