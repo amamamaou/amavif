905d768a28d89be61aac58c04ac888b342e798d5
amamamaou <41187086+amamamaou@users.noreply.github.com>
2025-06-04 16:21:46 +0900

    Updated auto-import settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { Features } from 'lightningcss'
@@ -12,10 +11,8 @@ const host = process.env.TAURI_DEV_HOST
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
     Components({
+      dts: false,
       resolvers: [ElementPlusResolver()],
     }),
   ],
